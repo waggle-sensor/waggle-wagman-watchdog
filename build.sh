d799459de40e0b4e6ccf677663f50ae0e07acf4a
Omar Zorob <omar.zorob@yahoo.com>
2020-12-04 16:23:08 -0600

    SAGE-611 Switched to systemd timer to run service, max run time before service is killed, other very minor problems

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ GIT_SHA=$(git rev-parse --short HEAD)
 DIRTY=$([[ -z $(git status -s) ]] || echo '-dirty')
 VERSION=${BASE_VERSION}-${GIT_SHA}${DIRTY}
 
-docker build -t pi_build .
-docker run --rm --privileged \
--v `pwd`:/output/ -e VERSION=$VERSION pi_build ./release.sh  
+docker build -t wagman .
+docker run --rm \
+-v `pwd`:/output/ -e VERSION=$VERSION wagman ./release.sh  
